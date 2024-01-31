da12f74af911c9ab699b5334c7aab44df306b66b
MereyZhaksylyk <158323604+MereyZhaksylyk@users.noreply.github.com>
2024-01-31 23:48:21 +0600

    Add files via upload

---
+++ 2lab.py
@@ -0,0 +1,92 @@
+#Task1
+print("Hello World")
+
+#Task2
+if 5 > 2:
+    print("YES")
+
+#Task3
+#This is a comment
+
+#Task
+""""
+This is a comment
+written in 
+more than just one line
+"""
+
+Task5
+carname = "Volvo"
+
+Task6
+x = 50 
+
+#Task7
+x = 5
+y = 10
+print(x+y)
+
+#Task8
+x = 5
+y = 10
+z = x+y
+print(z)
+
+
+Task9
+x, y, z = "Orange", "Banana", "Cherry"
+
+Task10
+x = y = z = "Orange"
+
+Task11
+def myfunc():
+    global x
+    x = "fantastic"
+
+Task12
+x = 5
+print(type(x))
+int
+
+Task13
+x = "Hello World"
+print(type(x))
+str
+
+Task14
+x = 20.5
+print(type(x))
+float
+
+Task15
+x = ["apple", "banana", "cherry"]
+print(type(x))
+list
+
+Task16
+x = ("apple", "banana", "cherry")
+print(type(x))
+tuple
+
+Task17
+x = {"name" : "John", "age" : 36}
+print(type(x))
+dict
+
+Task18
+x = True
+print(type(x))
+bool
+
+Task19
+x = 5
+x = float(x)
+
+Task20
+x = 5.5
+x = int(x)
+
+Task21
+x = 5
+x = complTask(x)
